8c0a01c20eccd4a9c5414a03ac364c80d5fb4d49
Armando <armandocrescencio343@gmail.com>
2025-06-14 11:40:46 -0600

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-  
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import morgan from 'morgan'
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+const PORT = process.env.PORT || 3000
+
+app.get('/api', (_req, res) => {
+  res.send('Hello, World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+// export default app;
+// export { app }; // Export the app for testing or further use
